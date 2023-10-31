5b79aaec2edba5aa0598dc62f642cb3db10ac4a1
Alessandro Pellegrino <alessandro.pellegrino9494@gmail.com>
2023-10-31 08:25:13 +0100

    .

--- apps/static/assets/js/base.js
+++ apps/static/assets/js/base.js
@@ -1,19 +1,3 @@
-/*
-
-=========================================================
-* Volt Pro - Premium Bootstrap 5 Dashboard
-=========================================================
-
-* Product Page: https://themesberg.com/product/admin-dashboard/volt-bootstrap-5-dashboard
-* Copyright 2021 Themesberg (https://www.themesberg.com)
-
-* Designed and coded by https://themesberg.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. Please contact us to request a removal. Contact us if you want to remove it.
-
-*/
 
 "use strict";
 const d = document;
